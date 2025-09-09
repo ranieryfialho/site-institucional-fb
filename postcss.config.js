8be08aaeaeee5038d2d2fe1a76ac7bcd5f13093c
Raniery Fialho <ranieryfialho@gmail.com>
2025-09-09 18:25:56 -0300

    ajustes de tailwind e vite

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,15 @@
+import tailwindcss from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
+import postcssNesting from 'postcss-nesting'
+import cssnano from 'cssnano'
+
+const isProd = process.env.NODE_ENV === 'production'
+
 export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: [
+    postcssNesting(),
+    tailwindcss(),
+    autoprefixer(),
+    ...(isProd ? [cssnano({ preset: 'default' })] : []),
+  ],
 }
